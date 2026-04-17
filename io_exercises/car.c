1be761f3ae05710a6a4c9ba045cf2a4ba11166af
alper-bora <alperbora2006@gmail.com>
2026-04-17 17:13:26 +0300

    Implemented state machine in car exercise.

--- io_exercises/car.c
+++ io_exercises/car.c
@@ -1,13 +1,60 @@
 #include <stdio.h>
 
-void emergencyBrake(float *speedAddress){
-    *speedAddress = 0.0;
+enum states
+{
+    EMERGENCY_BRAKE = 2,
+    CRUISING = 1,
+    IDLE = 0
+};
+
+typedef struct
+{
+    float speed;
+    float obstacle_distance;
+    enum states current_state;
+} TelemetryPacket;
+
+void emergencyBrake(TelemetryPacket *car){
+    car->speed = 0.0;
+}
+
+void state_idle(TelemetryPacket *car)
+{
+    if (car->speed >= 10) car->speed -= 10;
+    else car->speed = 0;
+}
+
+void state_cruising(TelemetryPacket *car)
+{
+    car->speed += 10;
 }
 
 int main() {
-    float momentary_speed = 85.5;
-    printf("Momentary speed: %f\n", momentary_speed);
-    emergencyBrake(&momentary_speed);
-    printf("Momentary speed: %f", momentary_speed);
+    void (*state_machine[]) (TelemetryPacket *) = {state_idle, state_cruising, emergencyBrake};
+    TelemetryPacket myCar = {0.0, 0.0, 0};
+    while (1)
+    {
+        printf("The distance to obstacle: ");
+        scanf("%f", &myCar.obstacle_distance);
+        if (myCar.obstacle_distance > 50)
+        {
+            myCar.current_state = CRUISING;
+            state_machine[myCar.current_state](&myCar);
+            printf("The speed is: %.2f\n", myCar.speed);
+        }
+        else if (myCar.obstacle_distance < 10)
+        {
+            myCar.current_state = EMERGENCY_BRAKE;
+            state_machine[myCar.current_state](&myCar);
+            printf("The speed is: %.2f\n", myCar.speed);
+            break;
+        }
+        else
+        {
+            myCar.current_state = IDLE;
+            state_machine[myCar.current_state](&myCar);
+            printf("The speed is: %.2f\n",myCar.speed);
+        }
+    }
     return 0;
 }
